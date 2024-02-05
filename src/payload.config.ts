15c691a01cefc9252c8be2cda1e40d0753cd117a
VovaLyapota <rimidalvatopayl@gmail.com>
2024-02-05 13:24:49 +0200

    add product collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,11 @@
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
+import dotenv from "dotenv";
 import path from "path";
 import { buildConfig } from "payload/config";
+import { Products } from "./collections/Products/Products";
 import { Users } from "./collections/Users";
-import dotenv from "dotenv";
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -12,7 +13,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products],
   routes: {
     admin: "/sell",
   },
