c904a5bc1ba4bd0b992ab74d4667f8f8f61058ce
VovaLyapota <rimidalvatopayl@gmail.com>
2024-02-15 18:26:51 +0200

    .

--- dist/config/index.js
+++ dist/config/index.js
@@ -5,20 +5,20 @@ exports.PRODUCT_CATEGORIES = [
     {
         label: "UI Kits",
         value: "ui_kits",
-        feachured: [
+        featured: [
             {
                 name: "Editor picks",
-                href: "#",
+                href: "/products?category=ui_kits",
                 imageSrc: "/nav/ui-kits/mixed.jpg",
             },
             {
                 name: "New Arrivals",
-                href: "#",
+                href: "/products?category=ui_kits&sort=desc",
                 imageSrc: "/nav/ui-kits/blue.jpg",
             },
             {
                 name: "Bestsellers",
-                href: "#",
+                href: "/products?category=ui_kits",
                 imageSrc: "/nav/ui-kits/purple.jpg",
             },
         ],
@@ -26,20 +26,20 @@ exports.PRODUCT_CATEGORIES = [
     {
         label: "Icons",
         value: "icons",
-        feachured: [
+        featured: [
             {
                 name: "Favorite Icon Picks",
-                href: "#",
+                href: "/products?category=icons",
                 imageSrc: "/nav/icons/picks.jpg",
             },
             {
                 name: "New Arrivals",
-                href: "#",
+                href: "/products?category=icons&sort=desc",
                 imageSrc: "/nav/icons/new.jpg",
             },
             {
                 name: "Bestselling Icons",
-                href: "#",
+                href: "/products?category=icons",
                 imageSrc: "/nav/icons/bestsellers.jpg",
             },
         ],
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -49,7 +49,7 @@ const NavItem = ({ isAnyOpen, isOpen, handleOpen, category }: NavItemProps) => {
               <div className="mx-auto max-w-7xl px-8">
                 <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
                   <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
-                    {category.feachured.map((item) => (
+                    {category.featured.map((item) => (
                       <div
                         key={item.name}
                         className="group relative text-base sm:text-sm"
